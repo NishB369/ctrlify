bdb65a057350465d6406ca44bbbe4abfca7421bb
nishb369 <nishbcodes@gmail.com>
2025-05-17 21:22:50 +0530

    Opening Animation Responsiveness Added

--- src/Components/OpeningAnimation/OpeningAnimation.js
+++ src/Components/OpeningAnimation/OpeningAnimation.js
@@ -1,10 +1,34 @@
-import React from "react";
-import video from "../../../public/media/videos/OpeningAnimationmWeb.mp4";
+import { useState, useEffect } from "react";
+import mWebvideo from "../../../public/media/videos/OpeningAnimationmWeb.mp4";
+import webVideo from "../../../public/media/videos/OpeningAnimationWeb.mp4";
 
 const OpeningAnimation = ({ showContent }) => {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkScreenSize();
+
+    window.addEventListener("resize", checkScreenSize);
+
+    // why cleanup???
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   return (
-    <div className="h-full" style={{ display: showContent ? "none" : "block" }}>
-      <video src={video} autoPlay muted className="object-cover h-full" />
+    <div
+      className="h-full w-full"
+      style={{ display: showContent ? "none" : "block" }}
+    >
+      <video
+        src={isMobile ? mWebvideo : webVideo}
+        autoPlay
+        muted
+        className="object-cover h-full w-full"
+      />
     </div>
   );
 };
